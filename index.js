3ab8b72c402768db69a5aad09a6e4e60d9626d37
Blair McKenzie <shib71@gmail.com>
2013-08-12 07:00:30 +1000

    moved deepresolve into module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // class has extendWith function which returns a new class
 // class returned by extendWith has extendWith class
 // extended with adds itself to results
-var util = require("util"), Q = require("q");
+var util = require("util"), Q = require("q"), deepResolve = require("deepresolve");
 
 module.exports = baseclass = function(){}
 
@@ -179,52 +179,5 @@ baseclass.hasPromise = hasPromise = function(val){
   }
 }
 
-baseclass.deepResolve = deepResolve = function(val,toJSON){
-    var queue = [];
-
-    toJSON = toJSON || false;
-
-    if (val && val.toJSON && toJSON){
-        return Q(val.toJSON());
-    }
-    else if (val && val.constructor == Array){
-        val.forEach(function(v,i){
-            queue.push(resolveChild(val,v,i,toJSON));
-        });
-    }
-    else if (val && val.constructor == Object){
-        for (var k in val){
-            queue.push(resolveChild(val,val[k],k,toJSON));
-        }
-    }
-
-    return Q.all(queue).then(function(){
-        return val;
-    });
-}
-
-function resolveChild(val,v,i,toJSON){
-    var result = true;
-
-    if (Q.isPromise(v)){
-        result = v.then(function(resolvedval){
-            if (toJSON && resolvedval && resolvedval.toJSON)
-                val[i] = resolvedval.toJSON();
-            else
-                val[i] = resolvedval;
-
-            if (val[i] && (val[i].constructor==Array || val[i].constructor==Object))
-                return deepResolve(val[i],toJSON);
-            else
-                return true;
-        });
-    }
-    else if (v && (v.constructor==Array || v.constructor==Object)){
-        result = deepResolve(v,toJSON);
-    }
-    else if (v && v.toJSON && toJSON){
-        val[i] = v.toJSON();
-    }
-
-    return result;
-}
+baseclass.deepResolve = deepResolve;
+baseclass.hasPromise = deepResolve.hasPromise;
